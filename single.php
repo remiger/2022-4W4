cb872fe33b6a1d837c0a6507a7fbdad61302cccd
Remi Gervais <e2095138@cmaisonneuve.qc.ca>
2022-02-14 11:00:48 -0500

    fin cours 02/14

---
+++ single.php
@@ -0,0 +1,25 @@
+<?php get_header() ?>
+<main class="site__main">
+    <h1>--------------------single.php--------------------</h1>
+    <?php if(have_posts()): ?>
+        <section class="cours">
+            <?php the_post(); ?>
+            <article class="cours__carte">
+                <?php
+                    $titre = get_the_title();
+                    $titreCourt = substr(get_the_title(), 8);
+                    $titreCourt = substr($titreCourt, 0, strrpos($titreCourt, '('));
+
+                    $posDebutHeures = strrpos($titre, '(') + 1;
+                    $posFinHeures = strrpos($titre, ')');
+                    $nbHeures = substr($titre, $posDebutHeures, $posFinHeures - $posDebutHeures);
+                ?>
+                <h2 class="carte__titre"><?= $titreCourt; ?></h2>
+                <p class="carte__code"><?= substr(get_the_title(), 0, 7) ?></p>
+                <p><?= $nbHeures; ?></p>
+                <p class="carte__description"><?= get_the_excerpt() ?></p>
+            </article>
+        </section>
+    <?php endif; ?>
+</main>
+<?php get_footer() ?>
