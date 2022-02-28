f5250df7458ec9405e55a5176109d7419c6e7d07
Remi Gervais <e2095138@cmaisonneuve.qc.ca>
2022-02-28 14:13:23 -0500

    ajustements grosseurs et alignements icones svg menu barre

--- header.php
+++ header.php
@@ -42,10 +42,7 @@
     <label for="chk-burger" id="burger">
         <svg width="35px" height="35px" xmlns="http://www.w3.org/2020/svg" viewBox="0 0 20 20" fill="currentColor" color="#000"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
     </label>
-    <?php $icone = '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#fff"><path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z"></path></svg>'; ?>
+    <?php $icone = '<svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#fff"><path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z"></path></svg>'; ?>
     <?php wp_nav_menu(array('menu' => 'principal',
-                            "container" => "nav",
-                            "menu_class" => "header__menu",
-                            "container_class" =>"site__header__nav",
                             "link_before" => $icone)); ?>
 </section>
