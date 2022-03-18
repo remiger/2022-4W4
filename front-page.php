e765c9828afedecc65d30516a573ae4623003bc7
Rémi Gervais <remger99@gmail.com>
2022-03-18 17:10:41 -0400

    s8-c2 RG Alignement du contenu dans principal et ACCUEIL page principale

--- front-page.php
+++ front-page.php
@@ -2,11 +2,13 @@
 <main class="principal">
 <!-- <h1>---- front-page.PHP ----</h1> -->
 <section class="animation">
-    <div class="animation__bloc">1</div>
-    <div class="animation__bloc">2</div>
-    <div class="animation__bloc">3</div>
-    <div class="animation__bloc">4</div>
-    <div class="animation__bloc">5</div>
+    <div class="animation__bloc">A</div>
+    <div class="animation__bloc">C</div>
+    <div class="animation__bloc">C</div>
+    <div class="animation__bloc">U</div>
+    <div class="animation__bloc">E</div>
+    <div class="animation__bloc">I</div>
+    <div class="animation__bloc">L</div>
 </section>
     <?php if (have_posts()):the_post(); ?>
     <h1><?php the_title(); ?></h1>
