370cdf8ceb916b520d3c6f9693a6bacb3296e6ec
Rémi Gervais <remger99@gmail.com>
2022-05-19 13:12:18 -0400

    ef-RG Ajout icones menu et animations sur les icones

--- template-atelier.php
+++ template-atelier.php
@@ -12,15 +12,18 @@
    <article class="atelier">
       <?php if(have_posts()):the_post(); ?>
         <h2><?php the_title(); ?></h2>
-        <section class="atelier__description"><?php the_field('description'); ?></section>
-        <p class="atelier__animateur"><?php the_field('animateur'); ?></p>
-        <p class="atelier__datedebut"><?php the_field('datedebut'); ?></p>
-        <p class="atelier__datefin"><?php the_field('datefin'); ?></p>
-        <p class="atelier__jourssemaine"><?php the_field('jourssemaine'); ?></p>
-        <p class="atelier__heuredebut"><?php the_field('heuredebut'); ?></p>
-        <p class="atelier__heurefin"><?php the_field('heurefin'); ?></p>
-        <p class="atelier__dureeseance"><?php the_field('dureeseance'); ?></p>
-        <p class="atelier__local"><?php the_field('local'); ?></p>
+        <h3>Description de l'atelier</h3><br>
+        <p>L'animateur de l'atelier : </p><p class="atelier__animateur"><?php the_field('animateur'); ?></p><br>
+        <p>L'atelier sera donné au local : </p><p class="atelier__local"><?php the_field('local'); ?></p>
+        <p class="atelier__description"><?php the_field('description'); ?></p><br>
+        <br>
+        <h3>Horaire et dates de l'atelier</h3><br>
+        <p>Durée de chacune des séances est de : </p><p class="atelier__dureeseance"><?php the_field('dureeseance'); ?></p><p>h</p><br>
+        <p>Date de début : </p><p class="atelier__datedebut"><?php the_field('datedebut'); ?></p><br>
+        <p>Date de fin : </p><p class="atelier__datefin"><?php the_field('datefin'); ?></p><br>
+        <p>La formation se donnera : </p><p class="atelier__jourssemaine"><?php the_field('jourssemaine'); ?></p><br>
+        <p>L'horaire : De </p><p class="atelier__heuredebut"><?php the_field('heuredebut'); ?></p><p> à </p><p class="atelier__heurefin"><?php the_field('heurefin'); ?></p><br>
+        
       <?php endif; ?>
     </article>
  </main>
